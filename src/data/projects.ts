d1951a695e49f1cccaf29bf8966326dbb781799f
Akshat Giri <akshatgiri@gmail.com>
2025-01-05 06:40:39 +0000

    Adding a list of projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,21 +6,52 @@ export type Project = {
 };
 
 const projects: Project[] = [
-  // {
-  //   title: "Puppetfactory",
-  //   techs: ["Next.js", "TypeScript"],
-  //   link: "https://puppetfactory.app/",
-  // },
-  // {
-  //   title: "Script to generate Next.js project",
-  //   techs: ["Shell"],
-  //   link: "https://github.com/MaeWolff/create-nextjs-app-bash-script",
-  // },
-  // {
-  //   title: "Dictionary App",
-  //   techs: ["Next.js", "react-query", "zod"],
-  //   link: "https://github.com/MaeWolff/dictionary-app",
-  // },
+  {
+    title: "Hiros.ai | AI Betting Agent",
+    techs: ["ai agents", "solidity", "nextjs"],
+    link: "https://hiros.vercel.app/",
+    isComingSoon: true
+  },
+  {
+    title: "Ekos.io | Website and Auction",
+    techs: ["viem", "solidity", "nextjs"],
+    link: "https://ekos.io/",
+  },
+  {
+    title: "CoinScout.xyz | Algorithmic Crypto Trading",
+    techs: ["typescript", "python", "rust", "pytorch", "etc"],
+    link: "https://x.com/summoningxyz"
+  },
+  {
+    title: "Banda.fun | Band the Panda Memecoin",
+    techs: ["rust", "solana"],
+    link: "https://banda.fun/"
+  },
+  {
+    title: "FlowMultisender.com - Multisender for the FlowEVM Chain",
+    techs: ["solidity", "react"],
+    link: "https://flowmultisender.com/",
+  },
+  {
+    title: "Solana Wallet Batch Manager | Open Source",
+    techs: ["typescript"],
+    link: "https://github.com/AkshatGiri/solana_batch_wallet_management",
+  },
+  {
+    title: "Kai | KeyboardAI - ChatGPT accessible from your keyboard",
+    techs: ["swift", "openai"],
+    link: "https://trykai.app/",
+  },
+  { 
+    title: "Summoning.xyz | Onchain multiplayer event",
+    techs: ["web3", "solidity", "nextjs"],
+    link: "https://x.com/summoningxyz"
+  },
+  { 
+    title: "Vaynersports Pass | NFT Drop",
+    techs: ["web3", "solidity", "nextjs"],
+    link: "https://opensea.io/collection/vaynersports-pass-vsp"
+  }
   // {
   //   title: "Portfolio / Template",
   //   techs: ["Astro", "TypeScript"],
